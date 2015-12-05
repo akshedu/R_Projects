ddec8334a69595c6ced62e1354eed20d49bbfdee
akshedu <akshedu@gmail.com>
2015-12-05 12:05:30 +0530

    Normalised graphs added in rice analysis

--- riceanalysis.R
+++ riceanalysis.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(ggplot2)
+library(reshape2)
 library(gridExtra)
 
 data <- fread("formattedrice.csv")
@@ -32,16 +33,17 @@ data_rainfall <- data_rainfall[,lapply(.SD,sum,na.rm=TRUE)]
 
 rainfall <- data.frame(2002:2011,as.numeric(data_rainfall))
 colnames(rainfall) <- c("Year","Annual_Total_Rainfall")
-rainfall <- ts(rainfall[,2], start=2002)
-rainfall_change <- rainfall/lag(rainfall,-1) - 1
+rainfall_ts <- ts(rainfall[,2], start=2002)
+rainfall_change <- rainfall_ts/lag(rainfall_ts,-1) - 1
 rainfall_change <- as.data.frame(as.numeric(rainfall_change))
 colnames(rainfall_change) <- "Value"
 rainfall_change["Year"] <- 2003:2011
 rainfall_change["Type"] <- rep("rainfall_change",9)
 
-avg_yearly <- data[,.(Yearly_Average = mean(Price, na.rm=TRUE)),by = .(year(Date))]
+avg_yearly <- data[,.(Yearly_Average_Price = mean(Price, na.rm=TRUE)),by = .(year(Date))]
 avg_yearly_2002_11 <- avg_yearly[year %between% c(2002,2011)]
-avg_yearly_ts <- ts(as.numeric(avg_yearly_2002_11[,Yearly_Average]),start=2002)
+setnames(avg_yearly_2002_11,"year","Year")
+avg_yearly_ts <- ts(as.numeric(avg_yearly_2002_11[,Yearly_Average_Price]),start=2002)
 avg_yearly_change <- avg_yearly_ts/lag(avg_yearly_ts, -1) - 1
 avg_yearly_change <- as.data.frame(as.numeric(avg_yearly_change))
 colnames(avg_yearly_change) <- "Value"
@@ -52,4 +54,14 @@ change_data <- rbind(rainfall_change,avg_yearly_change)
 
 r <- ggplot(change_data,aes(Year, Value, color = Type)) + geom_line() + geom_point()
 
+rainfall <- as.data.table(rainfall)
+rainfall_norm <- rainfall[,Annual_Total_Rainfall := scale(Annual_Total_Rainfall,center = min(Annual_Total_Rainfall),scale = max(Annual_Total_Rainfall)-min(Annual_Total_Rainfall))]
 
+avg_yearly_2002_11 <- as.data.table(avg_yearly_2002_11)
+avg_yearly_norm <- avg_yearly_2002_11[, Yearly_Average_Price := scale(Yearly_Average_Price,center = min(Yearly_Average_Price),scale = max(Yearly_Average_Price)-min(Yearly_Average_Price))]
+
+norm_change_data <- merge(rainfall_norm,avg_yearly_norm,by = "Year")
+
+norm_change_data_m <- melt(norm_change_data,id.vars="Year")
+
+s <- ggplot(norm_change_data_m,aes(Year,value,color = variable)) + geom_line() + geom_point()
